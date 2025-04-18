3353fb288ba4a90fc5ffbef5a4741fe177aa95c2
architbaurai <architbaurai11@gmail.com>
2025-04-18 16:05:29 +0530

    added distributed tracing via apm.

--- user-service/src/main/java/com/user/service/UserServiceApplication.java
+++ user-service/src/main/java/com/user/service/UserServiceApplication.java
@@ -3,13 +3,13 @@ package com.user.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.retry.annotation.EnableRetry;
-
+import co.elastic.apm.attach.ElasticApmAttacher;
 @SpringBootApplication
 @EnableFeignClients
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
+		ElasticApmAttacher.attach();
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
